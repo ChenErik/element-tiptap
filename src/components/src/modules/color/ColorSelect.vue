bfa975a023d316180ae91b748c563ff602322328
ChenErik <2392237608@qq.com>
2022-09-23 00:13:46 +0800

    feat:add color select

--- src/components/src/modules/color/ColorSelect.vue
+++ src/components/src/modules/color/ColorSelect.vue
@@ -1,4 +1,6 @@
 <script lang="ts" setup>
+import { inject, ref } from 'vue'
+import { editorKey } from '../../utils'
 import MenuItem from '../components/MenuItem.vue'
 interface Props {
   title: string
@@ -8,22 +10,45 @@ withDefaults(defineProps<Props>(), {
   title: '字体颜色',
   icon: 'font-color',
 })
+
+const editor = inject(editorKey)!()
+
+interface ColorList {
+  hex: string
+}
+function setColor(hex: string) {
+  editor.value.chain().focus().setColor(hex)
+}
+const colorList = ref<ColorList[]>([
+  {
+    hex: 'green',
+  },
+])
 </script>
 
 <template>
   <el-popover
     placement="bottom"
-    title="Title"
     :width="200"
     trigger="click"
-    content="this is content, this is content, this is content"
   >
     <template #reference>
       <MenuItem :title="title" :icon="icon" />
     </template>
+    <template #default>
+      <div class="color-box">
+        <div v-for="item of colorList" :key="item.hex" class="color-box_item" :style="{ backgroundColor: item.hex }" />
+      </div>
+    </template>
   </el-popover>
 </template>
 
-<style lang="less" scoped>
-
+<style lang="less">
+  .color-box {
+    &_item {
+      width: 30px;
+      height: 30px;
+      border-radius: 50%;
+    }
+  }
 </style>
